7de4c47831c21e62bc743f1dad39707c13c2547e
Matthias <matth.holler@gmail.com>
2026-05-22 12:07:44 +0200

    Fix Pokemon details 2.0

--- src/hooks/usePokemonDetail.js
+++ src/hooks/usePokemonDetail.js
@@ -45,7 +45,7 @@ async function fetchAbility(abilityName) {
   const nameFr = data.names?.find(n => n.language.name === 'fr')?.name || abilityName;
   const descFr = data.flavor_text_entries
     ?.filter(e => e.language.name === 'fr')
-    ?.pop()?.flavor_text?.replace(/[\n\f]/g, ' ') || '';
+    ?.pop()?.flavor_text?.replace(/\\n|\\f|[\n\f\r]/g, ' ') || '';
   const result = { nameFr, descFr };
   abilityCache.set(abilityName, result);
   return result;
